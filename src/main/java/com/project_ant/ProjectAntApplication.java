89381fe26a3a619b395e3a322c80a71e0b67d40c
VanillaLatte1 <alsgur0331@gmail.com>
2025-09-15 21:44:25 +0900

    init

---
+++ src/main/java/com/project_ant/ProjectAntApplication.java
@@ -0,0 +1,13 @@
+package com.project_ant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAntApplication.class, args);
+    }
+
+}
